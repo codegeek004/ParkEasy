7969284921f0cb724456fb62205f275d36e6df0c
codegeek004 <yashvaishnav1404@gmail.com>
2025-12-30 12:48:49 +0530

    added databse_url for postgres configurations

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 import mysql.connector
-db_config1 = {
-        'host' : 'db',
-        'user' : 'root',
-        'password' : 'root',
-        'database' : 'parking'
+import psycopg2
+from decouple import config
+"""db_config1 = {
+        'host' : config('host', cast=str),
+        'user' : config('user', cast =str),
+        'password' : config('password', cast=str),
+        'database' : config('database', cast=str),
         }
-db = mysql.connector.connect(**db_config1)
+"""
+DATABASE_URL = config("DATABASE_URL") 
+#db = mysql.connector.connect(**db_config1)
+db = psycopg.connect(DATABASE_URL)
 cursor = db.cursor(buffered=True)
 
 
